57f985124cb329820648bb1280f699320b00771d
william <williamoony@gmail.com>
2021-10-20 01:36:37 +0800

    rename

--- src/test/java/org/lucener/test/TestEntity.java
+++ src/test/java/org/lucener/test/TestEntity.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.beykery.lucener.*;
 import org.lucener.*;
 import org.lucener.test.util.Mappers;
 
